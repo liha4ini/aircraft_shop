93b35cb708bbf62aacd188c6cc17075ec00c899b
liha4ini <liha4ev.sergey@gmail.com>
2022-08-10 08:13:51 +0300

    added redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/home-page';
+import { paths } from './paths';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={ <HomePage /> } />
+                <Route path={ paths.home } element={ <HomePage /> } />
             </Routes>
         </BrowserRouter>
     );
